448a9814be09cb9bdad66083166a2ded95347fa7
Ben <bftingle@ucsc.edu>
2022-02-19 22:24:56 -0800

    152 and 217 DESTROYED

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -32,6 +32,17 @@ public class PlayerController : MonoBehaviour
         }
 
         gameplayActions = controls.Gameplay;
+    }
+
+    private void Update()
+    {
+        movement.ReceiveInput(horizontalInput);
+        mouseLook.ReceiveInput(mouseInput);
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
 
         // groundMovement.[action].performed += context => do something
         gameplayActions.Move.performed += ctx => horizontalInput = ctx.ReadValue<Vector2>();
@@ -58,21 +69,62 @@ public class PlayerController : MonoBehaviour
         };
     }
 
-    private void Update()
-    {
-        movement.ReceiveInput(horizontalInput);
-        mouseLook.ReceiveInput(mouseInput);
-    }
+    private void OnDisable() {
+        controls.Disable();
 
-    private void OnEnable()
-    {
-        controls.Enable();
+        // groundMovement.[action].performed += context => do something
+        gameplayActions.Move.performed -= ctx => horizontalInput = ctx.ReadValue<Vector2>();
+
+        gameplayActions.Jump.performed -= _ => movement.OnJumpPressed();
+
+        gameplayActions.MouseX.performed -= ctx => mouseInput.x = ctx.ReadValue<float>();
+        gameplayActions.MouseY.performed -= ctx => mouseInput.y = ctx.ReadValue<float>();
+
+        gameplayActions.Shoot.performed -= _ => playerAbilities.newTarget();
+        gameplayActions.Abilities1.performed -= _ => playerAbilities.castSpell(0);
+        gameplayActions.Abilities2.performed -= _ => playerAbilities.castSpell(1);
+        gameplayActions.Abilities3.performed -= _ => playerAbilities.castSpell(2);
+        gameplayActions.Shield.performed -= _ => playerAbilities.castShield();
+        gameplayActions.Escape.performed -= _ => {
+            leaveButton.SetActive(!leaveButton.activeSelf);
+            if (leaveButton.activeSelf) {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            } else {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+        };
     }
 
     private void OnDestroy()
     {
-        controls.Disable();
-    }
+        if (enabled) {
+            controls.Disable();
 
- 
+            // groundMovement.[action].performed += context => do something
+            gameplayActions.Move.performed -= ctx => horizontalInput = ctx.ReadValue<Vector2>();
+
+            gameplayActions.Jump.performed -= _ => movement.OnJumpPressed();
+
+            gameplayActions.MouseX.performed -= ctx => mouseInput.x = ctx.ReadValue<float>();
+            gameplayActions.MouseY.performed -= ctx => mouseInput.y = ctx.ReadValue<float>();
+
+            gameplayActions.Shoot.performed -= _ => playerAbilities.newTarget();
+            gameplayActions.Abilities1.performed -= _ => playerAbilities.castSpell(0);
+            gameplayActions.Abilities2.performed -= _ => playerAbilities.castSpell(1);
+            gameplayActions.Abilities3.performed -= _ => playerAbilities.castSpell(2);
+            gameplayActions.Shield.performed -= _ => playerAbilities.castShield();
+            gameplayActions.Escape.performed -= _ => {
+                leaveButton.SetActive(!leaveButton.activeSelf);
+                if (leaveButton.activeSelf) {
+                    Cursor.lockState = CursorLockMode.None;
+                    Cursor.visible = true;
+                } else {
+                    Cursor.lockState = CursorLockMode.Locked;
+                    Cursor.visible = false;
+                }
+            };
+        }
+    }
 }
