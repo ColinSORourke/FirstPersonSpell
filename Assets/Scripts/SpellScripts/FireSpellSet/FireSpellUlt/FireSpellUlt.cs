c7b45933b6f1107a159aabfd82f4e2f9b15dc46d
vincecheng714 <viwcheng@ucsc.edu>
2022-03-07 16:12:46 -0800

    implemented fire spell set + icons

---
+++ Assets/Scripts/SpellScripts/FireSpellSet/FireSpellUlt/FireSpellUlt.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "FireSpellUlt", menuName = "ScriptableObjects/FireSpells/FireSpellUlt", order = 1)]
+public class FireSpellUlt : baseSpellScript
+{
+    // Start is called before the first frame update
+    override public void onCastSpecific(Transform Player, Transform Target, int slot)
+    {
+        // None
+        Debug.Log("Cast Fire Spell Ult");
+    }
+
+    // Update is called once per frame
+    override public void onHit(Transform Player, Transform Target, int slot)
+    {
+        PlayerStateScript target = Target.GetComponent<PlayerStateScript>();
+        target.takeDamage(damage);
+        if (hitParticle != null)
+        {
+            var particleBurst = Instantiate(hitParticle, Target);
+            particleBurst.Emit(10);
+        }
+        float duration = 10.0f;
+        target.applyAura(Player, aura_A, duration);
+        if (hitParticle != null)
+        {
+            Instantiate(hitParticle, Target);
+        }
+    }
+}
