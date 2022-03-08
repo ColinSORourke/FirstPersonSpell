970539188e664ba3ffd758732d78ee4034de7146
Dennis Zabluda <denzabluda@gmail.com>
2022-03-07 21:40:14 -0800

    Update PlayerAbilities.cs

--- Assets/Scripts/Player/PlayerAbilities.cs
+++ Assets/Scripts/Player/PlayerAbilities.cs
@@ -94,51 +94,6 @@ public class PlayerAbilities : MonoBehaviour
                 myUI.updateCast(castTime/totalCastTime);
             }
         }
-
-        
-        //parseInput();
-    }
-
-    public void parseInput()
-    {
-        /*
-        if (Input.GetKeyUp(KeyCode.Mouse0)){
-            newTarget();
-        }
-        */
-
-        float valid;
-        float range = 0.0f;
-        bool tar = currTarget != -1;
-        
-
-        /*
-        if (Input.GetKeyUp("1") && valid != -1.0f ){
-            this.cast(0, valid);
-        }
-        */
-        valid = myState.validCast(1, tar, range);
-        /*
-        if (Input.GetKeyUp("2") && valid != -1.0f){
-            this.cast(1, valid);
-        }
-        */
-        valid = myState.validCast(2, tar, range);
-
-        /*
-        if (Input.GetKeyUp("3") && valid != -1.0f){
-            this.cast(2, valid);
-        }
-
-        if (Input.GetKeyUp(KeyCode.F)){
-            Debug.Log("Pressed F");
-            if (myState.currShields> 0){
-                myState.shieldDur = myState.shieldTime;
-                myState.currShields -= 1;
-                myUI.displayShield();
-            }
-        }
-        */
     }
 
     public void castSpell(int slot){
