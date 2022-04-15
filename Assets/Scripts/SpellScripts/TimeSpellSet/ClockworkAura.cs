77e72dceb65badf39bee4608c89c4a5bd739fcd2
Colin O'Rourke <csorourk@ucsc.edu>
2022-04-15 15:39:34 -0700

    Revert "Update ClockworkAura.cs"
    This reverts commit 1423015bf02edf349f3bede59b8752ad9c8d6ffa.

--- Assets/Scripts/SpellScripts/TimeSpellSet/ClockworkAura.cs
+++ Assets/Scripts/SpellScripts/TimeSpellSet/ClockworkAura.cs
@@ -9,8 +9,7 @@ public class ClockworkAura : baseAuraScript
     {
         Debug.Log("Clockwork applied");
         PlayerStateScript pSpeed = Player.GetComponent<PlayerStateScript>();
-        //pSpeed.moveSpeed += 2.0f;
-        pSpeed.changePlayerMovementSpeedRpc(pSpeed.moveSpeed + 2.0f);
+        pSpeed.moveSpeed += 2.0f;
     }
 
     override public void onTick(Transform Player, Transform Target, int stack, int tickNum)
@@ -22,8 +21,7 @@ public class ClockworkAura : baseAuraScript
     {
         Debug.Log("Frostbite expired");
         PlayerStateScript pSpeed = Player.GetComponent<PlayerStateScript>();
-        //pSpeed.moveSpeed -= 2.0f;
-        pSpeed.changePlayerMovementSpeedRpc(pSpeed.moveSpeed - 2.0f);
+        pSpeed.moveSpeed -= 2.0f;
     }
 
     override public void onStack(Transform Player, Transform Target, int stack)
@@ -32,8 +30,7 @@ public class ClockworkAura : baseAuraScript
         if (stack == 2)
         {
             PlayerStateScript pSpeed = Target.GetComponent<PlayerStateScript>();
-            //pSpeed.moveSpeed += 2.0f;
-            pSpeed.changePlayerMovementSpeedRpc(pSpeed.moveSpeed + 2.0f);
+            pSpeed.moveSpeed += 2.0f;
         }
 
     }
