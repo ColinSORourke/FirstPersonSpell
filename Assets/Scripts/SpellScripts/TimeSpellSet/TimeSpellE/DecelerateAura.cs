1f53f2878bc42c910ffe7b693b7e3f2be8f6e82d
Dennis Zabluda <denzabluda@gmail.com>
2022-04-15 15:28:57 -0700

    Update Player's Movement
    Made PlayerStateScript's moveSpeed a network variable.
    Modified other scripts to handle the changes.

--- Assets/Scripts/SpellScripts/TimeSpellSet/TimeSpellE/DecelerateAura.cs
+++ Assets/Scripts/SpellScripts/TimeSpellSet/TimeSpellE/DecelerateAura.cs
@@ -9,7 +9,8 @@ public class DecelerateAura : baseAuraScript
     {
         Debug.Log("Deceleration applied");
         PlayerStateScript tSpeed = Target.GetComponent<PlayerStateScript>();
-        tSpeed.moveSpeed -= 2.0f;
+        //tSpeed.moveSpeed -= 2.0f;
+        tSpeed.changePlayerMovementSpeedRpc(tSpeed.moveSpeed - 2.0f);
     }
 
     override public void onTick(Transform Player, Transform Target, int stack, int tickNum)
@@ -21,7 +22,8 @@ public class DecelerateAura : baseAuraScript
     {
         Debug.Log("Deceleration expired");
         PlayerStateScript tSpeed = Target.GetComponent<PlayerStateScript>();
-        tSpeed.moveSpeed += 2.0f;
+        //tSpeed.moveSpeed += 2.0f;
+        tSpeed.changePlayerMovementSpeedRpc(tSpeed.moveSpeed + 2.0f);
     }
 
     override public void onStack(Transform Player, Transform Target, int stack)
